3bd7eccdf32dc72cedf2af5c12f07b69ba664b3c
etoyz <2905226519@qq.com>
2021-10-24 16:06:59 +0800

    fix:企业用户数据显示异常

--- service/get_enterprise_list.php
+++ service/get_enterprise_list.php
@@ -5,17 +5,26 @@ $response = array(
     "data" => null
 );
 
-if (isset($_SESSION['usertype']) && $_SESSION['usertype'] == "manager") { // 已经登录
-    $query = $_GET['query'];
-    $from = ($_GET['page'] - 1) * $_GET['limit'];
-    $to = $_GET['page'] * $_GET['limit'];
-    $sql = "SELECT * FROM `enterprise` LIMIT " . $from . "," . $to . ";";
+if (isset($_SESSION['usertype'])) { // 已经登录
     require_once 'db.php';
-    $db = new DB();
-    $re = $db->query($sql);
-    //echo $re;
-    $response["status"] = "isLogin";
-    $response["data"] = $re->fetch_all(MYSQLI_ASSOC);
-    $response["code"] = 0;
+    if ($_SESSION['usertype'] == "manager") {
+        $query = $_GET['query'];
+        $from = ($_GET['page'] - 1) * $_GET['limit'];
+        $to = $_GET['page'] * $_GET['limit'];
+        $sql = "SELECT * FROM `enterprise` LIMIT " . $from . "," . $to . ";";
+        $db = new DB();
+        $re = $db->query($sql);
+        //echo $re;
+        $response["status"] = "isLogin";
+        $response["data"] = $re->fetch_all(MYSQLI_ASSOC);
+        $response["code"] = 0;
+    } else if ($_SESSION['usertype'] == "enterprise") {
+        $sql = "SELECT * FROM `enterprise` WHERE `loginid` = " . $_SESSION['loginid'] . ";";
+        $db = new DB();
+        $re = $db->query($sql);
+        $response["status"] = "isLogin";
+        $response["data"] = $re->fetch_all(MYSQLI_ASSOC);
+        $response["code"] = 0;
+    }
 }
 die(json_encode($response));
