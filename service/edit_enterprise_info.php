2871f078627ff99150fc9ebb6ffe3aa1e8941d69
etoyz <2905226519@qq.com>
2021-10-24 17:53:58 +0800

    fix:string bug

--- service/edit_enterprise_info.php
+++ service/edit_enterprise_info.php
@@ -6,15 +6,16 @@ $response = array(
 );
 if (isset($_SESSION['usertype'])) {
     $sql = "UPDATE `enterprise` 
-        SET `city` = $_POST[city], 
-            `province` = $_POST[province], 
-            `address` = $_POST[address], 
-            `numofsub` = $_POST[numofsub], 
-            `mainbus` = $_POST[mainbus], 
-            `industry` = $_POST[industry], 
-            `chargeperson` = $_POST[chargeperson], 
-            `phone` = $_POST[phone], 
-            `remark` = $_POST[name],
+        SET `city` = '$_POST[city]', 
+            `province` = '$_POST[province]', 
+            `name` = '$_POST[name]', 
+            `address` = '$_POST[address]', 
+            `numofsub` = '$_POST[numofsub]', 
+            `mainbus` = '$_POST[mainbus]', 
+            `industry` = '$_POST[industry]', 
+            `chargeperson` = '$_POST[chargeperson]', 
+            `phone` = '$_POST[phone]', 
+            `remark` = '$_POST[remark]',
             `status` = '已申报'
         WHERE `loginid` = '$_SESSION[loginid]';";
     require_once 'db.php';
