2f737eaa407f65eca5ef6d70026394a8609c15f7
etoyz <2905226519@qq.com>
2021-10-25 17:45:26 +0800

    feat:数据表格弹出输入框

---
+++ service/get_evaluating_indicator.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+$response = array(
+    "status" => 'notLogin',
+    "data" => null
+);
+
+if (isset($_SESSION['usertype'])) { // 已经登录
+    require_once 'db.php';
+    $sql = "DESCRIBE enterprise_data;";
+    $db = new DB();
+    $re = $db->query($sql);
+    $response["status"] = "isLogin";
+    $response["data"] = [];
+    $response["code"] = 0;
+    $re = $re->fetch_all(MYSQLI_ASSOC);
+    foreach ($re as $f) {
+        array_push($response["data"], array(
+            "indicator" => $f['Field']
+        ));
+    }
+}
+
+die(json_encode($response));
