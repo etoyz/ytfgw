54bb5e30d9f802e87eac18a036913e52878a68d2
etoyz <2905226519@qq.com>
2021-10-26 08:52:23 +0800

    opt:企业信息列表接口

--- service/get_enterprise_list.php
+++ service/get_enterprise_list.php
@@ -2,42 +2,36 @@
 session_start();
 $response = array(
     "status" => 'notLogin',
-    "data" => null
+    "data" => null,
+    "code" => 0
 );
 
 if (isset($_SESSION['usertype'])) { // 已经登录
     $response["status"] = "isLogin";
     require_once 'db.php';
     $sql = null;
+    $query = $_GET['query'];
+    $from = ($_GET['page'] - 1) * $_GET['limit'];
+    $to = $_GET['page'] * $_GET['limit'];
+    if ($_GET['status'] === '0')
+        $sql = "SELECT * FROM `enterprise` WHERE `status` IS NULL LIMIT " . $from . "," . $to . ";";
+    else if ($_GET['status'] === '1')
+        $sql = "SELECT * FROM `enterprise` WHERE `status` = '已申报' LIMIT " . $from . "," . $to . ";";
+    else if ($_GET['status'] === '-1')
+        $sql = "SELECT * FROM `enterprise` LIMIT " . $from . "," . $to . ";";
+    $db = new DB();
+    $re = $db->query($sql);
+    //echo $re;
+    $all_data = $re->fetch_all(MYSQLI_ASSOC);
     if ($_SESSION['usertype'] == "manager") {
-        $query = $_GET['query'];
-        $from = ($_GET['page'] - 1) * $_GET['limit'];
-        $to = $_GET['page'] * $_GET['limit'];
-        if ($_GET['status'] === '0')
-            $sql = "SELECT * FROM `enterprise` WHERE `status` IS NULL LIMIT " . $from . "," . $to . ";";
-        else if ($_GET['status'] === '1')
-            $sql = "SELECT * FROM `enterprise` WHERE `status` = '已申报' LIMIT " . $from . "," . $to . ";";
-        else if ($_GET['status'] === '-1')
-            $sql = "SELECT * FROM `enterprise` LIMIT " . $from . "," . $to . ";";
-        $db = new DB();
-        $re = $db->query($sql);
-        //echo $re;
-        $response["data"] = $re->fetch_all(MYSQLI_ASSOC);
-        $response["code"] = 0;
+        $response["data"] = $all_data;
     } else if ($_SESSION['usertype'] == "enterprise") {
-        if ($_GET['status'] === '0')
-            $sql = "SELECT * FROM `enterprise` WHERE `loginid` = " . $_SESSION['loginid'] . "
-                    WHERE `status` = NULL;";
-        else if ($_GET['status'] === '1')
-            $sql = "SELECT * FROM `enterprise` WHERE `loginid` = " . $_SESSION['loginid'] . "
-                    WHERE `status` = '已申报';";
-        else if ($_GET['status'] === '-1')
-            $sql = "SELECT * FROM `enterprise` WHERE `loginid` = " . $_SESSION['loginid'] . ";";
-        $db = new DB();
-        $re = $db->query($sql);
-        $response["data"] = $re->fetch_all(MYSQLI_ASSOC);
-        $response["code"] = 0;
+        for ($i = 0; $i < count($all_data); $i++) {
+            if ($all_data[$i]["loginid"] = $_SESSION['loginid']) {
+                $response["data"] = [$all_data[$i]];
+                break;
+            }
+        }
     }
 }
-//var_dump($sql);
 die(json_encode($response));
