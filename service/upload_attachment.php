350760a812a6bc03438e4efea672632ba062b90a
etoyz <2905226519@qq.com>
2021-10-29 15:25:47 +0800

    feat:上传附件API对接

--- service/upload_attachment.php
+++ service/upload_attachment.php
@@ -1,8 +1,31 @@
 <?php
 session_start();
 
-mkdir("../uploads/" . $_SESSION['loginid']);
-move_uploaded_file($_FILES['file']['tmp_name'],
-    "../uploads/" . $_SESSION['loginid'] . "/附件_" . $_POST['indicator'] . "_" . $_FILES['file']['name']);
+$response = array(
+    "code" => 0,
+    "msg" => "",
+    "data" => null
+);
 
-$_SESSION[''] = 1;
+set_error_handler(function ($errno, $errstr, $errfile, $errline) {
+    // error was suppressed with the @-operator
+    if (0 === error_reporting()) {
+        return false;
+    }
+
+    throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+});
+
+try {
+    mkdir("../uploads/" . $_SESSION['loginid']);
+} catch (Exception $e) {
+}
+ini_set("upload_max_filesize", "10M"); //TODO It does not work on some system
+try {
+    $response['data'] = move_uploaded_file($_FILES['file']['tmp_name'],
+        "../uploads/" . $_SESSION['loginid'] . "/附件_" . $_POST['indicator'] . "_" . $_FILES['file']['name']);
+} catch (Exception $e) {
+    $response['error'] = json_encode($e);
+}
+
+die(json_encode($response));
