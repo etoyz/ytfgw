742b0e39286650da0066521b0263a01957c49527
etoyz <2905226519@qq.com>
2021-10-30 13:30:03 +0800

    fix:only enterprise can submit_data

--- service/can_user_submit_data.php
+++ service/can_user_submit_data.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if (isset($_SESSION['usertype'])) {
+if (isset($_SESSION['usertype']) && $_SESSION['usertype'] === "enterprise") {
     if ($_SESSION['status'] === 1 || $_SESSION['status'] === 3)  // 首次申报 或者 退回
         die("1");
 }
