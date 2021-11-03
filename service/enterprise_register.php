1bf009152b730b8ccad28da8d5e3681e7a7516df
etoyz <2905226519@qq.com>
2021-11-03 21:16:35 +0800

    feat:企业密码哈希

--- service/enterprise_register.php
+++ service/enterprise_register.php
@@ -18,7 +18,8 @@ if ($re->num_rows == 1) {
 
 
 $sql = "INSERT INTO `enterprise` (loginid,loginpw,name,status) 
-        VALUES ('{$_POST['loginid']}','{$_POST['loginpw']}','{$_POST['name']}', 0)";
+        VALUES ('{$_POST['loginid']}','" . password_hash($_POST['loginpw'], PASSWORD_DEFAULT)
+    . "','{$_POST['name']}', 0)";
 $re = $db->query($sql);
 if ($re) {
     $arr['regSuccess'] = true;
