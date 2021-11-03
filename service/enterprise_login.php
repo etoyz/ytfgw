1a632586b54265dab64626d4e1e5bf4dcc04d2c4
etoyz <2905226519@qq.com>
2021-11-03 21:55:06 +0800

    fix:small bug

--- service/enterprise_login.php
+++ service/enterprise_login.php
@@ -7,16 +7,17 @@ $arr = array(
 //$response = json_decode(file_get_contents('php://input'));
 
 require_once 'db.php';
-$sql = "SELECT `loginpw` FROM `enterprise` WHERE `loginid` = '{$_POST['loginid']}';";
+$sql = "SELECT `loginpw`,`status` FROM `enterprise` WHERE `loginid` = '{$_POST['loginid']}';";
 $db = new DB();
 $re = $db->query($sql);
 
 if ($re->num_rows == 1) { // 后台校验成功
-    if (password_verify($_POST['loginpw'], $re->fetch_array()['loginpw'])) {
+    $data = $re->fetch_assoc();
+    if (password_verify($_POST['loginpw'], $data['loginpw'])) {
         $arr['status'] = 'isLogin';
         $_SESSION['usertype'] = "enterprise";
         $_SESSION['loginid'] = $_POST['loginid'];
-        $_SESSION['status'] = mysqli_fetch_assoc($re)['status']; // 0刚注册未完善信息 1已完善信息未申报数据 2已申报数据审核中 3已审核但是退回 4已审核通过
+        $_SESSION['status'] = $data['status']; // 0刚注册未完善信息 1已完善信息未申报数据 2已申报数据审核中 3已审核但是退回 4已审核通过
     }
 }
 
