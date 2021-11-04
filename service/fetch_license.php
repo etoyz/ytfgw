a2d3ef622d5e23e47417687d43a9c7c237c2b57c
etoyz <2905226519@qq.com>
2021-11-04 20:40:42 +0800

    feat:manager可查看营业执照

--- service/fetch_license.php
+++ service/fetch_license.php
@@ -5,17 +5,23 @@ $response = array(
 );
 
 session_start();
+$user = null;
+if ($_SESSION['usertype'] === "enterprise") {
+    $user = $_SESSION['loginid'];
+} else {
+    $user = $_GET['user'];
+}
 require_once "define_error_handler_to_catch_warnings.php";
 try {
-    $allFile = scandir("../uploads/$_SESSION[loginid]");
+    $allFile = scandir("../uploads/$user");
 } catch (Exception $e) {
 //    var_dump($e);
     $allFile = array();
 }
 require_once "common.php";
 if (fetch_indicator_attachment_name("企业营业执照", $allFile) != "未上传")
-    $response['license'] = "../uploads/$_SESSION[loginid]/" . fetch_indicator_attachment_name("企业营业执照", $allFile);
+    $response['license'] = "../uploads/$user/" . fetch_indicator_attachment_name("企业营业执照", $allFile);
 if (fetch_indicator_attachment_name("下属企业营业执照", $allFile) != "未上传")
-    $response['license_sub'] = "../uploads/$_SESSION[loginid]/" . fetch_indicator_attachment_name("下属企业营业执照", $allFile);
+    $response['license_sub'] = "../uploads/$user/" . fetch_indicator_attachment_name("下属企业营业执照", $allFile);
 
 die(json_encode($response));
