8fe5dc0cf0af8955792d607d93d20a1c66042871
etoyz <2905226519@qq.com>
2021-11-05 19:02:18 +0800

    opt:取消首页，用户、管理员登录用两个链接

--- service/logout.php
+++ service/logout.php
@@ -1,4 +1,7 @@
 <?php
 session_start();
+if ($_SESSION['usertype'] === "manager")
+    echo "<script>location.href='../web/manage_login.html'</script>";
+else
+    echo "<script>location.href='../web/enterprise_login.html'</script>";
 session_destroy();
-echo "<script>location.href='../index.php'</script>";
