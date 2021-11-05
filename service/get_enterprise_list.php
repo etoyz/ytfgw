0ffc420f00fdacf4ca4463d4132f212c52553081
etoyz <2905226519@qq.com>
2021-11-05 13:07:40 +0800

    fix:fix a bug temporary

--- service/get_enterprise_list.php
+++ service/get_enterprise_list.php
@@ -10,30 +10,33 @@ $response = array(
 if (isset($_SESSION['usertype'])) { // 已经登录
     $response["status"] = "isLogin";
     require_once 'db.php';
-    $sql = null;
-    $sql2 = null;
-    $query = $_GET['query'];
-    $from = ($_GET['page'] - 1) * $_GET['limit'];
-    $to = $_GET['limit'];
-    if ($_GET['status'] === '-1') { // 全部
-        $sql = "SELECT * FROM `enterprise` WHERE `name` LIKE '%$query%' LIMIT " . $from . "," . $to . ";";
-        $sql2 = "SELECT COUNT(*) AS cnt FROM `enterprise` WHERE `name` LIKE '%$query%';";
-    } else if ($_GET['status'] === '0-4') { // 申报状态
-        $sql = "SELECT * FROM `enterprise` WHERE `name` LIKE '%$query%' AND `status` != 5 LIMIT " . $from . "," . $to . ";";
-        $sql2 = "SELECT COUNT(*) AS cnt FROM `enterprise` WHERE `name` LIKE '%$query%' AND `status` != 5;";
-    } else if ($_GET['status'] === '5') {// 评价状态
-        $sql = "SELECT * FROM `enterprise` WHERE `name` LIKE '%$query%' AND `status` = 5 LIMIT " . $from . "," . $to . ";";
-        $sql2 = "SELECT COUNT(*) AS cnt FROM `enterprise` WHERE `name` LIKE '%$query%' AND `status` = 5;";
-    }
     $db = new DB();
-    $re = $db->query($sql);
-//    echo $sql;
-    if ($_SESSION['usertype'] == "manager")
-        $response['count'] = $db->query($sql2)->fetch_assoc()['cnt'];
-    $all_data = $re->fetch_all(MYSQLI_ASSOC);
     if ($_SESSION['usertype'] == "manager") {
+        $sql = null;
+        $sql2 = null;
+        $query = $_GET['query'];
+        $from = ($_GET['page'] - 1) * $_GET['limit'];
+        $to = $_GET['limit'];
+        if ($_GET['status'] === '-1') { // 全部
+            $sql = "SELECT * FROM `enterprise` WHERE `name` LIKE '%$query%' LIMIT " . $from . "," . $to . ";";
+            $sql2 = "SELECT COUNT(*) AS cnt FROM `enterprise` WHERE `name` LIKE '%$query%';";
+        } else if ($_GET['status'] === '0-4') { // 申报状态
+            $sql = "SELECT * FROM `enterprise` WHERE `name` LIKE '%$query%' AND `status` != 5 LIMIT " . $from . "," . $to . ";";
+            $sql2 = "SELECT COUNT(*) AS cnt FROM `enterprise` WHERE `name` LIKE '%$query%' AND `status` != 5;";
+        } else if ($_GET['status'] === '5') {// 评价状态
+            $sql = "SELECT * FROM `enterprise` WHERE `name` LIKE '%$query%' AND `status` = 5 LIMIT " . $from . "," . $to . ";";
+            $sql2 = "SELECT COUNT(*) AS cnt FROM `enterprise` WHERE `name` LIKE '%$query%' AND `status` = 5;";
+        }
+        $re = $db->query($sql);
+//    echo $sql;
+        if ($_SESSION['usertype'] == "manager")
+            $response['count'] = $db->query($sql2)->fetch_assoc()['cnt'];
+        $all_data = $re->fetch_all(MYSQLI_ASSOC);
         $response["data"] = $all_data;
     } else if ($_SESSION['usertype'] == "enterprise") {
+        $sql = "SELECT * FROM `enterprise`;";
+        $all_data = $db->query($sql)->fetch_all(MYSQLI_ASSOC);
+
         for ($i = 0; $i < count($all_data); $i++) {
             if ($all_data[$i]["loginid"] == $_SESSION['loginid']) {
                 $response["data"] = [$all_data[$i]];
