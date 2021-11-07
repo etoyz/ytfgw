bb59909e32cebea552905303e4996542ba2993d2
etoyz <2905226519@qq.com>
2021-11-07 14:53:01 +0800

    opt:PDF附件直接查看，而不必下载

--- service/attachment_view.php
+++ service/attachment_view.php
@@ -2,15 +2,15 @@
 session_start();
 if (isset($_SESSION['loginid'])) {  // 验证登录
     if ($_SESSION['usertype'] == "manager") {  // 管理员
-        header('Content-Type: application/octet-stream');
-        header('Content-Disposition: attachment; filename=' . basename($_GET['path']) . '');
+        header('Content-Type: application/pdf');
+        header('Content-Disposition: inline;');
 
         readfile($_GET['path']);
     } else {
         preg_match("/(?<=^..\/uploads\/).*(?=\/附件_.*$)/", $_GET['path'], $file_owner); //获取文件拥有者
         if ($_SESSION['loginid'] == $file_owner[0]) {
-            header('Content-Type: application/octet-stream');
-            header('Content-Disposition: attachment; filename=' . basename($_GET['path']) . '');
+            header('Content-Type: application/pdf');
+            header('Content-Disposition: inline;');
 
             readfile($_GET['path']);
         }
