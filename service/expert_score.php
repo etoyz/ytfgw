4330b9f46ba119e54ca065ae3236d3ac4c641f8e
etoyz <2905226519@qq.com>
2021-11-23 15:53:26 +0800

    feat:专家打分

---
+++ service/expert_score.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+
+if (isset($_SESSION['loginid']) && $_SESSION['usertype'] == "manager" && $_SESSION['privilege'] == '0') {
+    require_once 'db.php';
+    $db = new DB();
+
+    $sql = "UPDATE `enterprise` set `expert_score` = '" . $db->escape($_POST['expert_score']) . "' WHERE `loginid` = '$_POST[loginid]'";
+
+    $re = $db->query($sql);
+    if ($re === true)
+        die("ok");
+
+    die($re);
+} else
+    die("无权限");
