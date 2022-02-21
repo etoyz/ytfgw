466e9b40f7464fea54b3c221cac0e27f8c39095d
etoyz <2905226519@qq.com>
2022-02-21 12:39:11 +0800

    fix: a small error

--- service/get_manager_name.php
+++ service/get_manager_name.php
@@ -3,8 +3,10 @@
  * 获取管理员名称
  */
 session_start();
-require_once '../include/db.php';
-$db = new DB();
-$sql = "SELECT `name` FROM `manager` WHERE `loginid` = '" . $db->escape($_SESSION['loginid']) . "';";
-$re = $db->query($sql);
-die($re->fetch_array()['name']);
+if (isset($_SESSION['loginid'])) {
+    require_once '../include/db.php';
+    $db = new DB();
+    $sql = "SELECT `name` FROM `manager` WHERE `loginid` = '" . $db->escape($_SESSION['loginid']) . "';";
+    $re = $db->query($sql);
+    die($re->fetch_array()['name']);
+}
