ac198eb5afcd8125fcf8e0efef06f627be9839c3
etoyz <2905226519@qq.com>
2022-02-21 14:45:23 +0800

    opt: 权限控制

--- service/can_user_control_system.php
+++ service/can_user_control_system.php
@@ -3,7 +3,7 @@
  * 用户是否可以控制系统
  */
 session_start();
-if (isset($_SESSION['usertype']) && $_SESSION['usertype'] == "manager" && $_SESSION['privilege'] == "0")
+if (isset($_SESSION['usertype']) && $_SESSION['usertype'] == "manager" && $_SESSION['privilege'] == "0" && $_SESSION['type'] != 'expert')
     die("1");
 else
     die("0");
