87f1d07c784a04eee6586224b8d0a4e179abe05b
etoyz <2905226519@qq.com>
2023-02-01 17:48:52 +0800

    根据客户要求修改

--- 2023/service/attachment_view.php
+++ 2023/service/attachment_view.php
@@ -7,20 +7,20 @@ session_start();
 require "../include/common.php";
 
 if (is_login()) {  // 验证登录
+    // 构造相对路径
+    $indicator = $_GET['indicator'];
+    if (has_permission_admin())   // 管理员
+        $loginid = $_GET['loginid'];  // 管理员可看指定人的
+    else  // 非管理员
+        $loginid = $_SESSION['loginid']; // 非管理员只能看自己的
+
+    if (!is_attachment_exist($loginid, $indicator)) // 若附件不存在
+        die("<h1>用户未上传！</h1><h2>应该上传的材料: $indicator</h2>");
+
+    $url = "../uploads/$loginid/" . fetch_attachment_name($loginid, $indicator);
     // 响应头，在浏览器内查看即可
     header('Content-Type: application/pdf');
     header('Content-Disposition: inline;');
-
-    // 构造相对路径
-    $indicator = $_GET['indicator'];
-    if (has_permission_admin()) {  // 管理员
-        $loginid = $_GET['loginid'];  // 管理员可看指定人的
-        $url = "../uploads/$loginid/" . fetch_attachment_name($loginid, $indicator);
-    } else { // 非管理员
-        $loginid = $_SESSION['loginid']; // 非管理员只能看自己的
-        $url = "../uploads/$loginid/" . fetch_attachment_name($loginid, $indicator);
-    }
-
     // 响应体
     readfile($url);
 } else
--- 2023/service/get_score.php
+++ 2023/service/get_score.php
@@ -15,31 +15,54 @@ if (has_permission_admin_super() | has_permission_admin_expert()) { // 超管和
 
     $user = $_GET['user'];
     $db = new DB();
-    $sql = "DESCRIBE enterprise_score;";
-    $sql2 = "SELECT * FROM enterprise_score WHERE `loginid` = '" . $db->escape($user) . "' AND `type` = '0';";
-    $sql3 = "SELECT * FROM enterprise_score WHERE `loginid` = '" . $db->escape($user) . "' AND `type` = '1';";
-    $re = $db->query($sql);
-    $re2 = $db->query($sql2);
-    $re3 = $db->query($sql3);
+    $sql_fetch_indicators = "DESCRIBE enterprise_score;";
+    $sql_fetch_values0 = "SELECT * FROM enterprise_score WHERE `loginid` = '" . $db->escape($user) . "' AND `type` = '0';";
+    $sql_fetch_values1 = "SELECT * FROM enterprise_score WHERE `loginid` = '" . $db->escape($user) . "' AND `type` = '1';";
+    $re_indicators = $db->query($sql_fetch_indicators);
+    $re_values0 = $db->query($sql_fetch_values0);
+    $re_values1 = $db->query($sql_fetch_values1);
 
-    $re = $re->fetch_all(MYSQLI_ASSOC);
-    $re2 = mysqli_fetch_array($re2);
-    $re3 = mysqli_fetch_array($re3);
-    $i = 0;
-    foreach ($re as $f) {
-        if ($f['Field'] != "loginid" && $f['Field'] != "type") {
-            array_push($response["data"], array(
-                "indicator" => $f['Field'],
-                "score_0" => number_format($re2[$i], 2),
-                "score_1" => number_format($re3[$i], 2)
-            ));
-            // 修正，空值置为----
-            if ($re2[$i] === null)
-                $response["data"][sizeof($response["data"]) - 1]["score_0"] = "----";
-            if ($re3[$i] === null)
-                $response["data"][sizeof($response["data"]) - 1]["score_1"] = "----";
-        }
+    $re_indicators = $re_indicators->fetch_all(MYSQLI_ASSOC);
+    $re_values0 = mysqli_fetch_array($re_values0);
+    $re_values1 = mysqli_fetch_array($re_values1);
+
+    $indicators_disabled = ["loginid", "type"]; // 被禁用的列
+    $indicators_highlight1 = ["定量得分", "定性得分"]; // 特殊显示的列 level1
+    $indicators_highlight2 = ["总得分"]; // 特殊显示的列 level2
+
+    $i = -1;
+    foreach ($re_indicators as $f) { // 遍历各个指标
         $i++;
+        $indicator = $f['Field']; // 指标名
+        if (in_array($indicator, $indicators_disabled)) // 若是被禁用的列
+            continue;
+
+        // 得分
+        if ($re_values0[$i] === null)
+            $score_0 = "----";
+        else
+            $score_0 = number_format($re_values0[$i], 2);
+        if ($re_values1[$i] === null)
+            $score_1 = "----";
+        else
+            $score_1 = number_format($re_values1[$i], 2);
+
+        if (in_array($indicator, $indicators_highlight1)) { // 若是特殊显示的列 1
+            $indicator = "<span style='color: red;font-size: medium;'>$indicator</span>";
+            $score_0 = "<span style='color: red;font-size: medium;'>$score_0</span>";
+            $score_1 = "<span style='color: red;font-size: medium;'>$score_1</span>";
+        }
+        if (in_array($indicator, $indicators_highlight2)) { // 若是特殊显示的列 2
+            $indicator = "<span style='color: red;font-size: medium;font-weight: bold'>$indicator</span>";
+            $score_0 = "<span style='color: red;font-size: medium;font-weight: bold'>$score_0</span>";
+            $score_1 = "<span style='color: red;font-size: medium;font-weight: bold'>$score_1</span>";
+        }
+
+        array_push($response["data"], array(
+            "indicator" => $indicator,
+            "score_0" => $score_0,
+            "score_1" => $score_1
+        ));
     }
     die(json_encode($response));
 } else
