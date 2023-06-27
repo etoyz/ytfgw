a2e4ae4697331a3a847da3d4e2ff4c7f389db46f
sleephea <zzs13325294824@outlook.com>
2023-06-27 08:48:36 +0800

    修改系统公告

--- 2023/service/get_announcement.php
+++ 2023/service/get_announcement.php
@@ -9,4 +9,22 @@ require "../include/verify_login.php";
 
 $a = (new INI("../app.ini"))->data['Function']['announcement'];
 $a = str_replace(["\r\n", "\r", "\n"], '<br/>', $a);
+
 die($a);
+//$ini = new INI("../app.ini");
+//
+//$a = $ini->data['Function']['warning'];
+//$a = str_replace(["\r\n", "\r", "\n"], '<br/>', $a);
+//
+//$b = $ini->data['Function']['announcement'];
+//$b = str_replace(["\r\n", "\r", "\n"], '<br/>', $b);
+//
+//$output = '';
+//
+//foreach ([$a, $b] as $value) {
+//    if (!empty($value)) {
+//        $output .= $value . '<br/>';
+//    }
+//}
+//
+//die($output);
